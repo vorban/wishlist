767d33a8f387e34208c118ff8f6125695b80d5c0
val <valentin@agence-webup.com>
2022-11-20 10:30:11 +0100

    fix(vite): fix prod configuration

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/pages/*.js'
+                'resources/js/pages/access.js',
             ],
             refresh: true,
         }),
